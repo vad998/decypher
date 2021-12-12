b0603cf05e38042345d1630b007cc74f10d63f13
DESKTOP-8MOPUGF\vasil <vasillesha55@gmail.com>
2021-12-12 16:27:03 +0300

    Init

---
+++ index.js
@@ -0,0 +1,62 @@
+const multer = require("multer");
+const fs = require("fs");
+const nodersa = require("node-rsa");
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const PORT = process.env.PORT || 80;
+const app = express();
+
+app.use(express.json());
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.use("/", serveStatic(path.join(__dirname, "/public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post(
+  "/decrypt",
+  upload.fields([
+    {
+      name: "secret",
+      maxCount: 1,
+    },
+    {
+      name: "key",
+      maxCount: 1,
+    },
+  ]),
+  (req, res, next) => {
+    const files = req.files;
+
+    if (!files) {
+      const error = new Error("Please choose files");
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+
+    const privateKey = fs.readFileSync("./uploads/key", "utf8");
+    const decrypted = new nodersa(privateKey).decrypt(
+      fs.readFileSync("./uploads/secret"),
+      "utf8"
+    );
+
+    res.send(decrypted);
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
